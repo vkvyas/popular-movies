4b8916537bbc0a5e96ac9fad89dddad8e7d23169
Vishal Vyas <vishal@Vishals-MacBook-Pro.local>
2016-04-19 00:50:17 +0530

    Implemented settings activity

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.popularmovies;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.SupportMenuInflater;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,6 +29,8 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    public static final String TAG = "PM_TAG";
+    private StringBuilder apiUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +46,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        new FetchMoviesTask(recyclerView).execute((Void[]) null);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final String moviesSortOrder = prefs.getString("movies_sort_order", null);
+        apiUrl = new StringBuilder();
+        apiUrl.append("https://api.themoviedb.org/3");
+        if (TextUtils.isEmpty(moviesSortOrder) || moviesSortOrder.equalsIgnoreCase("Most popular")) {
+            apiUrl.append("/movie/popular");
+        } else {
+            apiUrl.append("/movie/top_rated");
+        }
+        apiUrl.append("?api_key=b6267c089cade0eccdb1732984aa12b9");
+        new FetchMoviesTask(recyclerView).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, apiUrl.toString());
+        Log.d(TAG, apiUrl.toString());
     }
 
-    class FetchMoviesTask extends AsyncTask<Void, String, List<MovieBean>> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class FetchMoviesTask extends AsyncTask<String, String, List<MovieBean>> {
 
         RecyclerView recyclerView;
         OkHttpClient okHttpClient;
-        String url = "https://api.themoviedb.org/3/movie/popular?api_key=b6267c089cade0eccdb1732984aa12b9";
 
         public FetchMoviesTask(RecyclerView recyclerView) {
             this.recyclerView = recyclerView;
         }
 
         @Override
-        protected List<MovieBean> doInBackground(Void... params) {
+        protected List<MovieBean> doInBackground(String... params) {
             try {
                 okHttpClient = new OkHttpClient();
                 Request request = new Request.Builder()
-                        .url(url)
+                        .url(params[0])
                         .build();
                 Response response = okHttpClient.newCall(request).execute();
                 String responseStr = response.body().string();
